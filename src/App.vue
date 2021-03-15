96d680c9893112c9de7f447b8732f9084c2c65a3
JanGoO-dev <55313789+JanGoO-dev@users.noreply.github.com>
2021-03-15 10:59:59 +0500

    First Commit to initialize danish-portfolio website porject

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  
+  <nav-bar />
+  <hero-section image="/static/images/image_1.jpg" />
+  <portfolio-section />
+  <resume-section />
+  <about-section />
+  <client-section />
+  <services-section />
+  <blog-section />
+  <contact-section />
+  <footer-section />
+
+</template>
+
+<script>
+
+import AboutSection from "./components/AboutSection.vue";
+import BlogSection from "./components/BlogSection.vue";
+import ClientSection from "./components/ClientSection.vue";
+import ContactSection from "./components/ContactSection.vue";
+import HeroSection from "./components/HeroSection.vue";
+import NavBar from "./components/NavBar.vue";
+import PortfolioSection from './components/PortfolioSection.vue';
+import ResumeSection from "./components/ResumeSection.vue";
+import ServicesSection from "./components/ServicesSection.vue";
+import FooterSection from "./components/FooterSection.vue";
+
+export default {
+  name: "App",
+  components: {
+    NavBar,
+    HeroSection,
+    PortfolioSection,
+    ResumeSection,
+    AboutSection,
+    ClientSection,
+    ServicesSection,
+    BlogSection,
+    ContactSection,
+    FooterSection
+  }
+};
+</script>
